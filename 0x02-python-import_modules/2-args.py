f74cd3d5149d83dcfe7d00c39e24109eee1e3938
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-04 10:03:05 +0200

    Adding 2-args.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argsLen = len(sys.argv) - 1
+
+    if argsLen == 1:
+        argString = "argument"
+    else:
+        argString = "arguments"
+
+    print("{} {}:".format(argsLen, argString))
+
+    for i in range(1, argsLen + 1):
+        print("{}: {}".format(i, sys.argv[i]))
