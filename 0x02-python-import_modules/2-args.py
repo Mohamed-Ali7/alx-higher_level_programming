ab1018072ae6b9ab86a8d0f9477ffdabc8501620
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-04 12:52:15 +0200

    Updating 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,13 @@ if __name__ == "__main__":
     argsLen = len(sys.argv) - 1
 
     if argsLen == 1:
-        argString = "argument"
+        argString = "argument:"
+    elif argsLen == 0:
+        argString = "arguments."
     else:
-        argString = "arguments"
+        argString = "arguments:"
 
-    print("{} {}:".format(argsLen, argString))
+    print("{} {}".format(argsLen, argString))
 
     for i in range(1, argsLen + 1):
         print("{}: {}".format(i, sys.argv[i]))
