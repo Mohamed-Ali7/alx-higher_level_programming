96f7279bc2800068ccfe8ef7409a7a5e14bfeabe
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-06 10:11:02 +0200

    Updating 13-is_palindrome.c file

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
+
+listint_t *reverse(listint_t *head);
 /**
  * is_palindrome - Checks if a singly linked list is a palindrome.
  * @head: Is a pointer to the pointer to the first element of the list
@@ -9,39 +11,44 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *cur = *head;
+	listint_t *temp = *head;
 	listint_t *jumper = *head;
-	int *list = NULL;
-	int size = 0;
 
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	while (cur != NULL)
-	{
-		cur = cur->next;
-		size++;
-	}
-
-	size /= 2;
-	cur = *head;
-	list = malloc(sizeof(int) * size);
 
 	while (jumper != NULL && jumper->next != NULL)
 	{
-		list[size - 1] = cur->n;
-		cur = cur->next;
 		jumper = jumper->next->next;
-		size--;
-	}
-	if (jumper != NULL)
 		cur = cur->next;
+	}
+	cur = reverse(cur);
 	while (cur != NULL)
 	{
-		if (list[size] != cur->n)
+		if  (cur->n != temp->n)
 			return (0);
 		cur = cur->next;
-		size++;
+		temp = temp->next;
 	}
-	free(list);
 	return (1);
+}
+/**
+ * reverse -  Reverses a listint_t linked list.
+ * @head: Is a pointer to the pointer to the first node of the list
+ * Return: A pointer to the first node of the reversed list
+ */
+listint_t *reverse(listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = head;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		head->next = prev;
+		prev = head;
+		head = next;
+	}
+	return (prev);
 }
