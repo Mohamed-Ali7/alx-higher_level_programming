3ddcacae907ed74df34d0f6bf2ebd7bb994e6242
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-06 07:35:47 +0200

    Updating 3-print_reversed_list_integer.py, and 9-max_integer.py files

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
-    max = 0
+    max_int = 0
     for i in my_list:
-        if max < i:
-            max = i
-    return max
+        if max_int < i:
+            max_int = i
+    return max_int
