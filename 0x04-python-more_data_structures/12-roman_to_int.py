0f62ea343a8e1663bf0452c88efade4d72976653
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-10 20:32:04 +0200

    Updating 12-roman_to_int.py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,9 +5,10 @@ def roman_to_int(roman_string):
     roman_nums =\
         {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
-
+    prev_result = 0
     for roman in roman_string:
+        if prev_result < roman_nums[roman]:
+            result -= prev_result * 2
         result += roman_nums[roman]
-    if (roman_string is "IV" or roman_string is "IX"):
-        result -= 2
+        prev_result = roman_nums[roman]
     return result
