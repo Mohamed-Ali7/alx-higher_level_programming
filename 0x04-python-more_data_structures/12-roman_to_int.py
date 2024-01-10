817cafa9e60f7a45803685fd040f609aedfe2215
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-10 20:03:09 +0200

    Adding 12-roman_to_int.py file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_nums =\
+        {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+
+    for index in range(len(roman_string)):
+        if index == 0 and roman_string[0] == "I":
+            result -= 1
+        else:
+            result += roman_nums[roman_string[index]]
+    return result
