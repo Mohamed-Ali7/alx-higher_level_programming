523ded210bf8d70d56264d2a3c77175e0d0001f3
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-22 21:20:47 +0200

    updating 1-safe_print_integer.py file

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,10 +2,8 @@
 
 def safe_print_integer(value):
 
-    print_elements = 0
-
     try:
         print("{:d}".format(value))
-    except TypeError:
+    except ValueError:
         return False
     return True
