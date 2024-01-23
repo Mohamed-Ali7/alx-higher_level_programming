727a0346adab70266b20f8670413805ede3396cd
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-24 01:07:53 +0200

    Updating 103-magic_class.py file

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
+
+"""
+This module is to practice classes and objects in Python
+This module contains Magicclass
+"""
+
 import math
 
 
 class MagicClass:
+
+    """
+    This class represnts data that will be compared to a ceratin bytecode
+    and these data are area and circumference of a circle
+
+    Attributes:
+        __radius (int or float): This attribute represents
+        the radius of the circle.
+    """
     def __init__(self, radius):
+        """This method initializes the instance
+
+        Args:
+            radius (int or float): Is the initializing value
+            of the radius attribute
+        """
 
         self.__radius = 0
 
@@ -13,7 +34,9 @@ class MagicClass:
             self.__radius = radius
 
     def area(self):
+        """This method is to return the area of the circle"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """This method is to return the circumference of the circle"""
         return 2 * math.pi * self.__radius
