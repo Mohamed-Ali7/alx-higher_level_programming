9cf5b537f35ede5298deff8aadf934f27729882f
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-23 20:01:08 +0200

    Updating 0-square.py file

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 """
-This module is to practice classes and objects in Python just by creating an empty class
+This module is to practice classes and objects in Python
+just by creating an empty class
 """
+
+
 class Square:
     """This Is an empty class"""
     pass
