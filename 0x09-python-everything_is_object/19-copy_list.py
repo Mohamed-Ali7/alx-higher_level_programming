5c0039114cfb7f09cf965f204d6f1d80411d9e0c
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-31 00:08:50 +0200

    Adding 19-copy_list.py file

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module contains copy_list() function"""
+
+
+def copy_list(l):
+    """
+    Returns a copy of a list
+
+    Args:
+        l (list): Is the list to return a copy of it
+    """
+
+    return l[:]
