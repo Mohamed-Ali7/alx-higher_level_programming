50e3104961b46e274d9a1b0ea0c30d34cc6e666a
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-01-31 02:11:51 +0200

    Updating 101-locked_class.py file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""This mdoule conatins LockedClass class"""
+
+
 class LockedClass:
+    """
+    This class prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called first_name.
+    """
 
     def __setattr__(self, name, value):
         error = f"'{__class__.__name__}' object has no attribute '{name}'"
