aa2805bb9137b9e5cd6ca3774ea5dfde9aa74869
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 16:51:29 +0200

    Updating 0-lookup.py file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
+"""This module contains the lookup() function"""
+
+
 def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+
     return dir(obj)
