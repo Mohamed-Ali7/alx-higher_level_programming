d49be2dc51c20d889b4648f8bd916b8355d4d68b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 19:34:58 +0200

    Adding 10-square.py file

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module contains Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class represents a square"""
+
+    def __init__(self, size):
+        """
+        The constructor of the class which intitalizes instances
+        Args:
+            size (int): Is the size of the sqquare
+        """
+        super().__init__(size, size)
+
+        self.__size = size
