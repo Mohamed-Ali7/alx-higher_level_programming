ca0e5a0660da41d7e3fe98e330cc0de470f5e77b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 17:46:39 +0200

    Updating 4-inherits_from.py  file

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     (directly or indirectly) from the specified class ; otherwise False.
     """
 
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
