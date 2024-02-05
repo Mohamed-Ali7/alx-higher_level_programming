4aaafe00bc32eaac148ea6359aba86131c69033b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 20:47:35 +0200

    Adding 100-my_int.py file

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,29 @@
+#!/bin/usr/python3
+
+"""This module contains MyInt class"""
+
+
+class MyInt(int):
+    """Custom class that inherits the (int) class"""
+
+    def __init__(self, my_int):
+        """Constructor that initializes instances"""
+
+        super().__init__()
+        self.my_int = my_int
+
+    def __eq__(self, obj):
+        """
+        Perform (=) operator in inverted order
+        and return true if the two objects are not equal
+        """
+
+        return self.my_int != obj
+
+    def __ne__(self, obj):
+        """
+        Perform (!=) operator in inverted order
+        and return true if the two objects are equal
+        """
+
+        return self.my_int == obj
