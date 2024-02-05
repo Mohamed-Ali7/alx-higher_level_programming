f9b6e3c2bf6804c8124440e2dcc5dc009edeedf6
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 21:39:34 +0200

    Updating 101-add_attribute.py file

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,7 +4,7 @@ def add_attribute(obj, att_name, att_value):
     """
     Adds a new attribute to an object if it’s possible
     Args:
-        obj (Any): Is the object to add the new attibute to
+        obj (object): Is the object to add the new attibute to
         att_name (str): Is the name of the new attribute
         att_value (Any): Is the value of the new attribute
 
@@ -12,7 +12,7 @@ def add_attribute(obj, att_name, att_value):
         TypeError: if the object cannot have new attribute
     """
 
-    setattr(obj, att_name, att_value)
-
-    if not hasattr(obj, att_name):
+    if obj.__class__.__module__ == 'builtins':
         raise TypeError("can't add new attribute")
+
+    setattr(obj, att_name, att_value)
