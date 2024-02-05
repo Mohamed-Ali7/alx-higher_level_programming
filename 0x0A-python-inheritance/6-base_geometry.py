3b0f8750ded7903d27182a097d9399d9bd4391fc
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-05 17:51:43 +0200

    Adding 6-base_geometry.py file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Thid module contains BaseGeometry class"""
+
+
+class BaseGeometry():
+    """This is a base class"""
+
+    def area(self):
+        """Raises:
+            Exception: if area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
