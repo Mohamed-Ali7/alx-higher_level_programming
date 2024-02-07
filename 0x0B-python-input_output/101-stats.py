f380cc25607ac9dce9049e48df9743bcfd20508f
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-07 05:19:03 +0200

    Updating 101-stats.py file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,16 +11,16 @@ i = 1
 status = {"200": 1, "301": 1, "400": 1, "401": 1,
           "403": 1, "404": 1, "405": 1, "500": 1}
 printable_status = {}
+
 try:
     for line in sys.stdin:
         words = line.split()
-        if len(words) == 0:
-            continue
-        status_code = words[-2]
-        total_size += int(words[-1])
-        if status_code in status:
-            printable_status[status_code] = status[status_code]
-            status[status_code] += 1
+        if len(words) >= 2:
+            status_code = words[-2]
+            total_size += int(words[-1])
+            if status_code in status:
+                printable_status[status_code] = status[status_code]
+                status[status_code] += 1
         if i == 10:
             print("File size: {:d}".format(total_size))
             for key, value in sorted(printable_status.items()):
