60fde3357b797adde1a69b6dc68dbeeb6468b3c3
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-07 04:35:58 +0200

    Updating 101-stats.py file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -7,23 +7,23 @@ import sys
 
 
 total_size = 0
+i = 0
 status = {"200": 1, "301": 1, "400": 1, "401": 1,
           "403": 1, "404": 1, "405": 1, "500": 1}
 printable_status = {}
 try:
-    while True:
-        for r in range(10):
-            line = sys.stdin.readline()
-            words = line.split()
-            status_code = words[-2]
-            total_size += int(words[-1])
-            if status_code in status:
-                printable_status[status_code] = status[status_code]
-                status[status_code] += 1
-
-        print(f"File size: {total_size}")
-        for key in sorted(printable_status):
-            print(f"{key}: {printable_status[key]}")
+    for line in sys.stdin:
+        words = line.split()
+        status_code = words[-2]
+        total_size += int(words[-1])
+        if status_code in status:
+            printable_status[status_code] = status[status_code]
+            status[status_code] += 1
+        if i != 0 and i % 10 == 0:
+            print(f"File size: {total_size}")
+            for key in sorted(printable_status):
+                print(f"{key}: {printable_status[key]}")
+        i += 1
 except KeyboardInterrupt as ex:
     print(f"File size: {total_size}")
     for key in sorted(printable_status):
