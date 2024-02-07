14459d7ae5a48b701b22889f2bffb95812321919
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-02-07 05:04:08 +0200

    Updating 101-stats.py file

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,30 +8,29 @@ import sys
 
 total_size = 0
 i = 1
-status = {"200": 0, "301": 0, "400": 0, "401": 0,
-          "403": 0, "404": 0, "405": 0, "500": 0}
+status = {"200": 1, "301": 1, "400": 1, "401": 1,
+          "403": 1, "404": 1, "405": 1, "500": 1}
+printable_status = {}
 try:
     for line in sys.stdin:
         words = line.split()
         status_code = words[-2]
         total_size += int(words[-1])
         if status_code in status:
+            printable_status[status_code] = status[status_code]
             status[status_code] += 1
         if i == 10:
             print("File size: {:d}".format(total_size))
-            for key, value in sorted(status.items()):
-                if value > 0:
-                    print("{}: {:d}".format(key, value))
+            for key, value in sorted(printable_status.items()):
+                print("{}: {:d}".format(key, value))
             i = 1
         else:
             i += 1
 
     print("File size: {:d}".format(total_size))
-    for key, value in sorted(status.items()):
-        if value > 0:
-            print("{}: {:d}".format(key, value))
+    for key, value in sorted(printable_status.items()):
+        print("{}: {:d}".format(key, value))
 except KeyboardInterrupt as ex:
     print("File size: {:d}".format(total_size))
-    for key, value in sorted(status.items()):
-        if value > 0:
-            print("{}: {:d}".format(key, value))
+    for key, value in sorted(printable_status.items()):
+        print("{}: {:d}".format(key, value))
