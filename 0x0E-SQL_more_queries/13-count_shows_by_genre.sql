68fd9053a28517d1f2b407d45a75962618e0ef6d
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-03-14 02:02:31 +0200

    Updating 13-count_shows_by_genre.sql file

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,7 +11,7 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name as genre, count(tv_show_genres.genre_id) as number_of_shows
+SELECT tv_genres.name AS genre, count(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY (tv_genres.name)
-ORDER by number_of_shows DESC
+ORDER BY number_of_shows DESC
