6120e709db55d961dde01bf946d63c6ca7fafa60
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-03-14 02:23:26 +0200

    Updating 14-my_genres.sql file

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -9,9 +9,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres JOIN tv_shows
+ON tv_genres.id = tv_show_genres.genre_id
+and tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name;
+ORDER BY tv_genres.name
