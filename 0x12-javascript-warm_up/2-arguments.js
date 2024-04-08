97cdc206122dcd58ae38aca90e3bca2671131b99
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-08 17:48:20 +0200

    Updating 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (argv.length > 2) {
+if (argv.length === 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
