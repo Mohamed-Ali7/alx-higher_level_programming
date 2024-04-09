ad268825546875dc4370a4999f36d5ed2ffc531b
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-09 18:19:41 +0200

    Adding 6-square.js file

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
