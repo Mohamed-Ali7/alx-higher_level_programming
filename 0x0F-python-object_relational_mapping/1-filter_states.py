fbbe29eaa3a60e73f2e30eebbbf86b513b387b94
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-15 00:43:36 +0200

    Updating 1-filter_states.py file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE(%s)", ("N%",))
+    cursor.execute("SELECT * FROM states WHERE name LIKE('N%')")
 
     for row in cursor.fetchall():
         print(row)
