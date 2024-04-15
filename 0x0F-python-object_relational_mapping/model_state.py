12292ff8e5451b76f3980c00cf0dc315822e5143
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-15 02:33:55 +0200

    Updating model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,14 +4,9 @@
 contains the class definition of a State and
 an instance Base = declarative_base():
 """
-import sys
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
 
 Base = declarative_base()
 
@@ -21,5 +16,5 @@ class State(Base):
     """This class represent the states table in hbtn_0e_6_usa daatabase"""
 
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
