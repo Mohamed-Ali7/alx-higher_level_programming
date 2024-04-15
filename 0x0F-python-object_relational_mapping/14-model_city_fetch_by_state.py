e56ceeae1e6652c49c5c3afeecaf2252501643ae
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-15 06:58:04 +0200

    Updating 14-model_city_fetch_by_state.py file

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
     from model_state import Base, State
     from model_city import City
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker, aliased
+    from sqlalchemy.orm import sessionmaker
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database), echo=True)
+                           .format(username, password, database))
 
     Session = sessionmaker(bind=engine)
 
