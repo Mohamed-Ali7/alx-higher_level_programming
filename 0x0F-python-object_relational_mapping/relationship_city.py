3f7d84381bd175e32c3dcb348c8f0d761d4e1f34
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-15 18:15:47 +0200

    Adding 100-relationship_states_cities.py, relationship_city.py, and relationship_state.py files

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""contains the class definition of a City and"""
+from sqlalchemy import Column, String, Integer, ForeignKey
+from relationship_state import Base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+
+    """This class represent the cities table in hbtn_0e_6_usa daatabase"""
+
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
