f9a5660a911cdfa91e3abd28d4d9d3436dba0329
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-27 00:32:05 +0300

    Updating 0-body_size.sh file

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # This script Takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 
-curl -Is $1 | grep 'Content-Length' | cut -d ' ' -f2
+curl -Is "$1" | grep 'Content-Length' | cut -d ' ' -f2
